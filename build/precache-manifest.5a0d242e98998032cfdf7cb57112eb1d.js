2ee6d4728afd52776521761310d19ec3c83270f1
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-31 15:54:15 -0600

    changed deploy settings in package.json

---
+++ build/precache-manifest.5a0d242e98998032cfdf7cb57112eb1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5be03ada7a0a1b07265e972bf99bc1",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "d3cc1122209911d3b531",
+    "url": "/clickyGame/static/css/main.8ee91cf7.chunk.css"
+  },
+  {
+    "revision": "04ef23bc96a2d7b6d4a1",
+    "url": "/clickyGame/static/js/2.5700d5ed.chunk.js"
+  },
+  {
+    "revision": "d3cc1122209911d3b531",
+    "url": "/clickyGame/static/js/main.898614d1.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
